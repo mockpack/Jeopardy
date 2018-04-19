57ada1eeffd6b032b29ee9c64c1860753cb4d016
Gavin Bading <gabading>
2018-04-19 15:12:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gabading/jeopardy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gabading.jeopardy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void accessSelect (View v) {
+        Intent intent = new Intent(this, categorySelect.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/gabading/jeopardy/categorySelect.java
@@ -0,0 +1,13 @@
+package com.example.gabading.jeopardy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class categorySelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_select);
+    }
+}
