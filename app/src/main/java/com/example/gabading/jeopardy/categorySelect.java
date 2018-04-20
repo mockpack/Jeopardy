70b7f1c288aa50f5dc28d0d5a70982e4ba5b2734
permulta <gavintbading37@gmail.com>
2018-04-20 14:09:26 -0500

    Added category onPress functions

--- app/src/main/java/com/example/gabading/jeopardy/categorySelect.java
+++ app/src/main/java/com/example/gabading/jeopardy/categorySelect.java
@@ -2,6 +2,7 @@ package com.example.gabading.jeopardy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class categorySelect extends AppCompatActivity {
 
@@ -9,5 +10,41 @@ public class categorySelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_select);
+    }
+
+    public void easyFilmPress (View v) {
+
+    }
+
+    public void mediumFilmPress (View v) {
+
+    }
+
+    public void hardFilmPress (View v) {
+
+    }
+
+    public void easyGamePress (View v) {
+
+    }
+
+    public void mediumGamePress (View v) {
+
+    }
+
+    public void hardGamePress (View v) {
+
+    }
+
+    public void easyMusicPress (View v) {
+
+    }
+
+    public void mediumMusicPress (View v) {
+
+    }
+
+    public void hardMusicPress (View v) {
+
     }
 }
